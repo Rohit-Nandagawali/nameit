7f2af2558053e5358f56cd90416540d155a61dd6
Rohit Sudhir Nandagawali <nandagawalirohit143@gmail.com>
2022-07-15 12:30:13 +0530

    Updates

---
+++ precache-manifest.a0a00fa5cecdbed5c6551e7a0f34d482.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5341bf076a222740a5f6",
+    "url": "/nameit/static/css/main.1e03407b.chunk.css"
+  },
+  {
+    "revision": "5341bf076a222740a5f6",
+    "url": "/nameit/static/js/main.5341bf07.chunk.js"
+  },
+  {
+    "revision": "a96d17faaf31aceddc2a",
+    "url": "/nameit/static/js/1.a96d17fa.chunk.js"
+  },
+  {
+    "revision": "3ba299ed607ab10ddb38",
+    "url": "/nameit/static/js/runtime~main.3ba299ed.js"
+  },
+  {
+    "revision": "77c58a77df209cc56ce870b91d9f3a04",
+    "url": "/nameit/index.html"
+  }
+];
